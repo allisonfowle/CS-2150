b2e0e74f2d806257d620c78ac87f62fd8a2f6d56
allisonfowle <89487018+allisonfowle@users.noreply.github.com>
2021-12-05 18:58:06 -0500

    commit lab 11

---
+++ lab11/postlab/puzzle.h
@@ -0,0 +1,21 @@
+/*
+Allison Fowle
+CS 2150 - acf6nb
+12/1/2021
+puzzle.h
+*/
+
+
+#ifndef PUZZLE_H
+#define PUZZLE_H
+
+#include <iostream>
+using namespace std;
+
+
+class puzzle {
+public:
+	
+};
+
+#endif
